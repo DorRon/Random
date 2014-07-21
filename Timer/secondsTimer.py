f8450274be67f3c2e7e35da30e0f16cd1cf00522
DorRon <dorxrondel@gmail.com>
2014-07-21 02:48:58 -0400

    Update secondsTimer.py
    Adjustments (floats) for greater timer accuracy.

--- Timer/secondsTimer.py
+++ Timer/secondsTimer.py
@@ -6,7 +6,10 @@ def getAlarmTime():
     seconds = input("Enter how many seconds you want to count down from: ")
     while seconds > 0:
         print seconds
-        sleep(1)
+        if seconds > 0 and seconds < 1: #counts down by actual seconds until var seconds > 0 and < 1, 
+            sleep(seconds)              #in which case it'll count down by remaining value, to insure
+        else:                           #that floats and decimals will be accounted for.
+            sleep(1)
         seconds -= 1
-        
+
 getAlarmTime()
